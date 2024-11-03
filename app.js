f01a90c57aa659bbca6397c0db60988f3fe488a0
Sophie Zhen <174782511+Sophie-Zhen@users.noreply.github.com>
2024-11-03 15:15:15 +0000

    Fix identation issues and grammar error

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// Only start the server if not in test mode
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
-});
+  }); 
 }
 
 module.exports = app;
